eec53d59920628441348fd2e3698fe215452f66b
Max_pain <yaseno933@gmail.com>
2023-12-02 14:25:19 +0200

    add the project

---
+++ calubeno/calubeno/Page1.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace calubeno
+{
+    /// <summary>
+    /// Interaction logic for Page1.xaml
+    /// </summary>
+    public partial class Page1 : Page
+    {
+        loginshiEntities db = new loginshiEntities ();
+        public Page1()
+        {
+            InitializeComponent();
+        }
+
+        private void pass_button(object sender, RoutedEventArgs e)
+        {
+            Page2 page = new Page2();
+            NavigationService.Navigate(page);
+        }
+
+        private void login_button(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                userr user = db.userrs.First(x => x.username == user_t.Text && x.password == pass_t.Text);
+                MessageBox.Show("loged in successfully");
+            }
+            catch
+            {
+                MessageBox.Show("wrong username or password");
+            }
+        }
+    }
+}
---
+++ calubeno/calubeno/Page2.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace calubeno
+{
+    /// <summary>
+    /// Interaction logic for Page2.xaml
+    /// </summary>
+    public partial class Page2 : Page
+    {
+        loginshiEntities db = new loginshiEntities();
+        public Page2()
+        {
+            InitializeComponent();
+        }
+
+        private void signup_button(object sender, RoutedEventArgs e)
+        {
+            userr user = new userr();
+            try
+            {
+                user.username = user_t.Text;
+                if (cpass_t.Text == pass_t.Text)
+                {
+                    user.password = pass_t.Text;
+                    db.userrs.Add(user);
+                    db.SaveChanges();
+                    MessageBox.Show("account created");
+                    Page1 page = new Page1();
+                    NavigationService.Navigate(page);
+                }
+                else
+                {
+                    MessageBox.Show("passwords doesn't match");
+                }
+            }
+            catch
+            {
+                MessageBox.Show("the username already exist");
+            }
+        }
+    }
+}
